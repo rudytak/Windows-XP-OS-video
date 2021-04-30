0bdc7dc58ba09d06dd59f2d74d10f1ea9f5b243c
rudytak <75335232+rudytak@users.noreply.github.com>
2021-05-01 01:13:58 +0200

    Loaded in data

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
-function setup() {
+var bg, icn, vid;
 
+function preload() {
+    bg = loadImage("background.png")
+    icn = loadImage("icn.png")
+    vid - createVideo("vid.mp4")
+
+    vid.hide();
+}
+
+function setup() {
+    print(bg, icn, vid)
 }
 
 function draw() {
