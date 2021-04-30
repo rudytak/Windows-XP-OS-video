0565616d29010a7c7ddf2585aafcc050b309d97a
rudytak <75335232+rudytak@users.noreply.github.com>
2021-05-01 01:41:52 +0200

    Background and icon

--- sketch.js
+++ sketch.js
@@ -3,15 +3,19 @@ var bg, icn, vid;
 function preload() {
     bg = loadImage("background.png")
     icn = loadImage("icn.png")
-    vid - createVideo("vid.mp4")
+    vid = createVideo("vid.mp4")
 
     vid.hide();
 }
 
 function setup() {
+    var c = createCanvas(innerWidth - 5, innerHeight - 5);
+    document.body.style.margin = "0px";
+    document.body.style.overflow = "hidden";
     print(bg, icn, vid)
 }
 
 function draw() {
-
+    image(bg, 0, 0, width, width / bg.width * bg.height)
+    image(icn, 327 * width / bg.width, 600 * height / bg.height, 150 * width / bg.width, 150 * width / bg.width)
 }
